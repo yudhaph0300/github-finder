49f54dd6a5306169edd6c240df2c36b4c07f98cc
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-11 15:35:32 +0700

    React Video - Working With The GitHub API | vid5

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults"
+
+function Home() {
+   return (
+     <>
+      {/* SEARCH COMPONENT */}
+      <UserResults/>
+     </>
+   )
+ }
+ 
+ export default Home
