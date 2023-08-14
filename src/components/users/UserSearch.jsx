ae5fcb6aeb0c3940dc5cb6eda350a4884646985e
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-14 14:48:19 +0700

    React Video - User Profile & Alerts | vid2

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,18 +1,20 @@
 import { useState, useContext } from "react"
 import GithubContext from "../../context/github/GithubContext"
+import AlertContext from "../../context/alert/AlertContext"
 
 
 function UserSearch() {
    const [text, setText] = useState('')
 
    const {users, searchUsers, clearUsers} = useContext(GithubContext)
+   const {setAlert} = useContext(AlertContext)
 
    const handleChange = (e) => setText(e.target.value)
 
    const handleSubmit = (e) => {
       e.preventDefault()
       if (text === "") {
-         alert('Please enter somethings')
+         setAlert('Please enter somethings', 'error')
       } else {
          searchUsers(text)
          setText('')
